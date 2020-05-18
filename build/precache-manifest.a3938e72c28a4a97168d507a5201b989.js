15c9bce9c090821a6d2bd860f869fe22dbc35d62
unknown <erdosancsa@gmail.com>
2020-05-18 11:59:08 +0200

    Initial Upload

---
+++ build/precache-manifest.a3938e72c28a4a97168d507a5201b989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6408e5cdf660ef5818d5977feada9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b9a149105b14048e285d",
+    "url": "./static/css/main.e24294e4.chunk.css"
+  },
+  {
+    "revision": "f9f1c85ed2dfb059483d",
+    "url": "./static/js/2.db39bea0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.db39bea0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9a149105b14048e285d",
+    "url": "./static/js/main.88a4618a.chunk.js"
+  },
+  {
+    "revision": "c999e1aafeacb3931724",
+    "url": "./static/js/runtime-main.9edb4eab.js"
+  }
+]);
